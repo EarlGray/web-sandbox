ad62cef2f8af99404555d97bcd54f8cb12374e9e
Dmytro S <dmytrish@gmail.com>
2015-01-13 19:52:21 +0200

    timestamps; notifications;

--- hellotornado.py
+++ hellotornado.py
@@ -84,5 +84,5 @@ application = tornado.web.Application([
 
 if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(8888);
     tornado.ioloop.IOLoop.instance().start()
