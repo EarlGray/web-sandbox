ff37955bf811ab124cf4ac6a23ab6fb70d142ad4
Dmytro S <dmytrish@gmail.com>
2016-06-14 00:42:38 +0300

    sqlog : FileUpdate.hs updates

--- nginx-sqlog/src/System/FileUpdate.hs
+++ nginx-sqlog/src/System/FileUpdate.hs
@@ -43,10 +43,10 @@ tryReceive ch = atomically $ STM.tryReadTChan ch
 
 subscribe :: FilePath -> IO UpdateChan
 subscribe fpath = do
-  evch <- chanNew
-  updch <- chanNew
-  forkIO $ tailf evch fpath
-  forkIO $ watcher evch updch
+  evch <- atomically $ STM.newTChan
+  updch <- atomically $ STM.newTChan
+  forkIO $ fswatcher evch fpath
+  forkIO $ updwatcher evch updch
   return updch
 
 watchWithResult :: FilePath -> (FileUpdate -> IO (Maybe r)) -> IO r
@@ -88,11 +88,8 @@ chanSend ch msg = atomically $ STM.writeTChan ch msg
 chanRecv :: STM.TChan a -> IO a
 chanRecv ch = atomically $ STM.readTChan ch
 
-chanNew :: IO (STM.TChan a)
-chanNew = atomically $ newTChan
-
-tailf :: TChan FS.Event -> FilePath -> IO ()
-tailf ch fpath = FS.withManager $ \fswatch -> do
+fswatcher :: TChan FS.Event -> FilePath -> IO ()
+fswatcher ch fpath = FS.withManager $ \fswatch -> do
     let (dir, fname) = splitFileName fpath
 
     t <- getCurrentTime
@@ -104,30 +101,14 @@ tailf ch fpath = FS.withManager $ \fswatch -> do
 
     forever $ sleepSeconds 10
 
-watcher :: TChan FS.Event -> TChan FileUpdate -> IO ()
-watcher inp out = go inp out M.empty
+updwatcher :: TChan FS.Event -> TChan FileUpdate -> IO ()
+updwatcher inp out = go inp out M.empty
   where
-    readnews fpath offset = do
-      exists <- doesFileExist fpath
-      case exists of
-        True -> IO.withBinaryFile fpath IO.ReadMode $ \h -> do
-                    IO.hSeek h IO.SeekFromEnd 0
-                    flen <- IO.hTell h
-                    let to_read' = fromInteger (flen - offset)
-                        (off, to_read) = if to_read' >= 0
-                                         then (offset, to_read')
-                                         else (0, fromInteger flen)
-                    IO.hSeek h IO.AbsoluteSeek off
-                    buf <- BL.hGet h to_read
-                    return (flen, buf)
-        False -> return (0, BL.empty)
-
-    update fpath state = do
-      let fname = takeFileName fpath
-          offset = fromMaybe 0 $ M.lookup fname state
-      (flen, buf) <- readnews fpath offset
-      let state' = M.alter (const $ Just flen) fname state
-      return (buf, state')
+    go inp out state = do
+      ev <- chanRecv inp
+      (news, state') <- handle ev state
+      chanSend out news
+      go inp out state'
 
     handle event state = do
       let fpath = FS.eventPath event
@@ -143,9 +124,26 @@ watcher inp out = go inp out M.empty
           -- perror "[%s] Removed\t%s\n" (hms t) fname
           return (BL.empty, M.delete fname state)
         
-    go inp out state = do
-      ev <- chanRecv inp
-      (news, state') <- handle ev state
-      chanSend out news
-      go inp out state'
+    update fpath state = do
+      let fname = takeFileName fpath
+          offset = fromMaybe 0 $ M.lookup fname state
+      (flen, buf) <- readnews fpath offset
+      let state' = M.alter (const $ Just flen) fname state
+      return (buf, state')
+
+    readnews fpath offset = do
+      exists <- doesFileExist fpath
+      case exists of
+        True -> IO.withBinaryFile fpath IO.ReadMode $ \h -> do
+                    IO.hSeek h IO.SeekFromEnd 0
+                    flen <- IO.hTell h
+                    let to_read' = fromInteger (flen - offset)
+                        (off, to_read) = if to_read' >= 0
+                                         then (offset, to_read')
+                                         else (0, fromInteger flen)
+                    IO.hSeek h IO.AbsoluteSeek off
+                    buf <- BL.hGet h to_read
+                    return (flen, buf)
+        False -> return (0, BL.empty)
+
 
